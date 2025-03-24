2711e8d594f5b368c0b08a6b26cdbb3063d192ba
Alina Abdullaeva <areon.aur@mail.ru>
2025-03-24 18:51:15 +0300

    Second change added to Main

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ class Main{
     public static void main(String[] args) {
         System.out.print("Это добавлено в ветке main");
         System.out.println("25.03.2025 12:30");
+        System.out.println("25.03.2025 12:33");
     }
 }
